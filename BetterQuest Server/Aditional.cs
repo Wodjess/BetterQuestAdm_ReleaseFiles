fe9834984ac9b6bdb19fa553a63ed9a3fb024436
Wodjess <74964006+Wodjess@users.noreply.github.com>
2022-06-21 23:45:08 +0300

    main

---
+++ BetterQuest Server/Aditional.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+namespace BetterQuest_Server
+{
+    class Client
+    {
+        const int port = 48657;
+        public static void Download(string message, string address = "51.124.219.208")
+        {
+            TcpClient client = null;
+            try
+            {
+                client = new TcpClient(address, port);
+                NetworkStream stream = client.GetStream();
+                // ввод сообщения
+                if (message != "")
+                {
+                    message = String.Format(message);
+                    // преобразуем сообщение в массив байтов
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    // отправка сообщения
+                    stream.Write(data, 0, data.Length);
+
+                    // получаем ответ
+                    data = new byte[64]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    message = builder.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        void Tasker(object sender, MouseEventArgs e)
+        {
+            MainGrid.Opacity = 0.8;
+            base.DragMove();
+            MainGrid.Opacity = 1;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
---
+++ BetterQuest Server/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System.Windows;
+namespace BetterQuest_Server
+{
+    public partial class MainWindow : Window
+    {
+        sbyte temp;
+        sbyte temp1 = 1;
+        string password;
+        System.Windows.Media.Effects.BlurEffect blurEffect = new System.Windows.Media.Effects.BlurEffect();
+        public MainWindow()
+        {
+            InitializeComponent();
+            blurEffect.Radius = 5;
+            MainGrid.Effect = blurEffect;
+        }
+        void IwasteMyFreeTime(object sender, RoutedEventArgs e)
+        {
+            if (temp1 == 1)
+            {
+                temp1 = 0;
+                PasswordFromBox.Text = "";
+            }
+        }
+
+        void UploadOnServer(object sender, RoutedEventArgs e)
+        {
+            Working();
+        }
+        void BasicInformation(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (PasswordFromBox.Text.Length >= 5 && IPAdressOfServer.Text.Length >= 4)
+                {
+                    password = PasswordFromBox.Text;
+                    ProtectorGrid.IsEnabled = false;
+                    StarterPage.IsEnabled = false;
+                    ProtectorGrid.Visibility = Visibility.Hidden;
+                    StarterPage.Visibility = Visibility.Hidden;
+                    blurEffect.Radius = 0;
+                }
+                else
+                {
+                    MessageBox.Show("Убедитесь в правильности введенных данных");
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void Working()
+        {
+            try
+            {
+                string ip = IPAdressOfServer.Text;
+                if (Box.Text != "")
+                {
+                    if (temp == 0)
+                    {
+                        Client.Download(password + Box.Text, ip);
+                        Block.Text = "Впишите описание файла";
+                        Box.Text = "";
+                        temp = 1;
+                    }
+                    else
+                    {
+                        if (temp == 1)
+                        {
+                            Client.Download(password + Box.Text, ip);
+                            Block.Text = "Залейте картинку на хост и вставте прямую ссылку на нее";
+                            Box.Text = "";
+                            temp = 2;
+                        }
+                        else
+                        {
+                            if (temp == 2)
+                            {
+                                Client.Download(password + Box.Text, ip);
+                                Block.Text = "Залейте приложение на хост и вставте прямую ссылку на него (если файлов несколько, то в один zip архив)";
+                                Box.Text = "";
+                                temp = 3;
+                            }
+                            else
+                            {
+                                if (temp == 3)
+                                {
+                                    Client.Download(password + Box.Text, ip);
+                                    Block.Text = "Впишите название файла";
+                                    Box.Text = "";
+                                    temp = 0;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка " + ex.ToString());
+                temp = 3;
+            }
+        }
+
+    }
+}
